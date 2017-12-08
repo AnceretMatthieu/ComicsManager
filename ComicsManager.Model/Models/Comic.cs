40a197294954ec91acf465ecc55d11b88bef86bc
Matthieu Anceret <diablogore@hotmail.com>
2017-12-08 11:20:31 +0100

    Ajout d'une entité File

--- ComicsManager.Model/Models/Comic.cs
+++ ComicsManager.Model/Models/Comic.cs
@@ -1,24 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Text;
 
 namespace ComicsManager.Model.Models
 {
     public class Comic : BaseEntity
     {
-        public Author Scenariste { get; set; }
-
-        public Author Dessinateur { get; set; }
-
-        public DateTime PublicationDate { get; set; }
-
-        public Editor Editeur { get; set; }
-        
-        public string ISBN { get; set; }
-
         [Required]
         public string Title { get; set; }
+
+        public string ISBN { get; set; }
 
         public int Cycle { get; set; }
 
@@ -27,5 +17,17 @@ namespace ComicsManager.Model.Models
         public int Note { get; set; }
 
         public string Couverture { get; set; }
+
+        public DateTime PublicationDate { get; set; }
+
+
+        public Author Scenariste { get; set; }
+
+        public Author Dessinateur { get; set; }
+        
+        public Editor Editeur { get; set; }        
+       
+
+        public File File { get; set; }
     }
 }
---
+++ ComicsManager.Model/Models/File.cs
@@ -0,0 +1,9 @@
+﻿namespace ComicsManager.Model.Models
+{
+    public class File : BaseEntity
+    {
+        public string Path { get; set; }
+
+        public string Type { get; set; }
+    }
+}
